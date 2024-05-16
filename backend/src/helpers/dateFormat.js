ed043d17c22a143a499430c5050eacb29e8edacd
lucas2007c <carneiro.g@aluno.ifsp.edu.br>
2024-05-16 15:28:22 -0300

    update date formats

--- backend/src/helpers/dateFormat.js
+++ backend/src/helpers/dateFormat.js
@@ -1,9 +1,11 @@
 const dateFormat = (obj) => {
     for (let field in obj) {
         if (field === 'start' || field === 'finish' || field === 'platinum') {
-            const date = obj[field].toISOString().split('T')[0].split('-')
-            const dateFormatted = `${date[2]}/${date[1]}/${date[0]}`
-            obj[field] = dateFormatted
+            if (obj[field] !== null) {
+                const date = obj[field].toISOString().split('T')[0].split('-')
+                const dateFormatted = `${date[2]}/${date[1]}/${date[0]}`
+                obj[field] = dateFormatted
+            }
         }
     }
     return obj
--- backend/src/middlewares/prismaDateFormat.js
+++ backend/src/middlewares/prismaDateFormat.js
@@ -2,10 +2,12 @@ const prismaDateFormat = (req, res, next) => {
     const body = req.body
     for (let field in body) {
         if (field === 'start' || field === 'finish' || field === 'platinum') {
-            const date = body[field].split('/')
-            const dateFormatted = `${date[2]}-${date[1]}-${date[0]}`
-            const prismaDate = `${dateFormatted}T00:00:00.000Z`
-            body[field] = prismaDate
+            if (field !== null) {
+                const date = body[field].split('/')
+                const dateFormatted = `${date[2]}-${date[1]}-${date[0]}`
+                const prismaDate = `${dateFormatted}T00:00:00.000Z`
+                body[field] = prismaDate
+            }
         }
     }
     next()
