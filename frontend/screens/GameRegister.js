79e33c57661b7e697d09ea59fb303eb705743829
lucas2007c <carneiro.g@aluno.ifsp.edu.br>
2024-05-16 17:47:31 -0300

    init game form

--- frontend/screens/GameRegister.js
+++ frontend/screens/GameRegister.js
@@ -1,9 +1,20 @@
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, StyleSheet, TextInput } from "react-native"
+import H1 from '../components/ui/H1.js'
 
 const GameRegister = () => {
     return (
         <View style={styles.container}>
-            <Text style={{ color: '#fff' }}>Cadastrar jogo</Text>
+            <H1 style={styles.label}>Url da capa</H1>
+            <TextInput
+                placeholder="Insira sua url..."
+                style={styles.txtinput}
+            />
+
+            <H1 style={styles.label}>Nome do jogo</H1>
+            <TextInput
+                placeholder="Insira o nome..."
+                style={styles.txtinput}
+            />
         </View>
     )
 }
@@ -11,10 +22,13 @@ const GameRegister = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#242A32',
-        padding: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: '#242A32'
+    },
+    label: {
+        fontSize: 20
+    },
+    txtinput: {
+        backgroundColor: '#444'
     }
 })
 
