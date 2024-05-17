1c4d65c3036072cc8f8850bb2b3bece837aaf28b
lucas2007c <carneiro.g@aluno.ifsp.edu.br>
2024-05-17 17:29:58 -0300

    fix prisma date format

--- backend/src/middlewares/prismaDateFormat.js
+++ backend/src/middlewares/prismaDateFormat.js
@@ -2,7 +2,7 @@ const prismaDateFormat = (req, res, next) => {
     const body = req.body
     for (let field in body) {
         if (field === 'start' || field === 'finish' || field === 'platinum') {
-            if (field !== null) {
+            if (body[field] !== null) {
                 const date = body[field].split('/')
                 const dateFormatted = `${date[2]}-${date[1]}-${date[0]}`
                 const prismaDate = `${dateFormatted}T00:00:00.000Z`
