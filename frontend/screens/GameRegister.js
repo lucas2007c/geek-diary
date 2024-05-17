8f9f05eb0f02379fdada916d08b0b9abd660533d
lucas2007c <carneiro.g@aluno.ifsp.edu.br>
2024-05-17 00:21:35 -0300

    update game form

--- frontend/screens/GameRegister.js
+++ frontend/screens/GameRegister.js
@@ -1,35 +1,92 @@
-import { View, Text, StyleSheet, TextInput } from "react-native"
+import { View, Text, StyleSheet, TextInput, ScrollView } from "react-native"
 import H1 from '../components/ui/H1.js'
 import { COLORS } from "../constants/constants"
+import { useState } from "react"
 
 const GameRegister = () => {
-    return (
-        <View style={styles.container}>
-            <H1 style={styles.label}>Url da capa</H1>
-            <TextInput
-                placeholder="Insira sua url..."
-                style={styles.txtinput}
-            />
+    const [txtName, setTxtName] = useState('')
+    const [txtUrl, setTxtUrl] = useState('')
+    const [txtStart, setTxtStart] = useState('')
+    const [txtStatus, setTxtStatus] = useState('')
+    const [txtNotes, setTxtNotes] = useState('')
 
-            <H1 style={styles.label}>Nome do jogo</H1>
-            <TextInput
-                placeholder="Insira o nome..."
-                style={styles.txtinput}
-            />
-        </View>
+    return (
+        <ScrollView style={styles.container}>
+            <View style={styles.field}>
+                <H1 style={styles.label}>Nome do jogo</H1>
+                <TextInput
+                    placeholder="Nome..."
+                    placeholderTextColor={COLORS.secondary}
+                    style={styles.txtinput}
+                    value={txtName}
+                    onChangeText={setTxtName}
+                />
+            </View>
+
+            <View style={styles.field}>
+                <H1 style={styles.label}>Url da capa</H1>
+                <TextInput
+                    placeholder="Insira sua url..."
+                    placeholderTextColor={COLORS.secondary}
+                    style={styles.txtinput}
+                    value={txtUrl}
+                    onChangeText={setTxtUrl}
+                />
+            </View>
+
+            <View style={styles.field}>
+                <H1 style={styles.label}>Data de ínicio</H1>
+                <TextInput
+                    style={styles.txtinput}
+                    value={txtStart}
+                    onChangeText={setTxtStart}
+                />
+            </View>
+
+            <View style={styles.field}>
+                <H1 style={styles.label}>Status</H1>
+                <TextInput
+                    style={styles.txtinput}
+                    value={txtStatus}
+                    onChangeText={setTxtStatus}
+                />
+            </View>
+
+            <View style={styles.field}>
+                <H1 style={styles.label}>Anotações {txtNotes.length}/1000</H1>
+                <TextInput
+                    style={[styles.txtinput, { height: 500, color: '#fff', fontSize: 20 }]}
+                    placeholder="Anote o que quiser..."
+                    placeholderTextColor={COLORS.secondary}
+                    multiline
+                    value={txtNotes}
+                    onChangeText={setTxtNotes}
+                />
+            </View>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: COLORS.background
+        backgroundColor: COLORS.background,
+        paddingHorizontal: 25
     },
     label: {
-        fontSize: 20
+        fontSize: 20,
+        marginBottom: 5
     },
     txtinput: {
-        backgroundColor: '#444'
+        backgroundColor: '#00334E',
+        color: '#fff',
+        // height: 30,
+        borderRadius: 3,
+        padding: 5,
+        fontSize: 17
+    },
+    field: {
+        marginVertical: 10
     }
 })
 
