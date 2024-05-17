bc4ab98c567474d87ffaa5abbae2d21a1bc9e4f6
lucas2007c <carneiro.g@aluno.ifsp.edu.br>
2024-05-17 17:48:07 -0300

    add req.body to logger

--- backend/src/middlewares/logger.js
+++ backend/src/middlewares/logger.js
@@ -1,5 +1,7 @@
 const logger = (req, res, next) => {
     console.log(`${req.method} ${req.originalUrl}`);
+    console.log(req.body);
+    console.log('--------------------------------');
     next();
 }
 
