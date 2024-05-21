e0c3cdcd66d60ccf61a107399b382d60a37b36b6
lucas2007c <carneiro.g@aluno.ifsp.edu.br>
2024-05-21 16:38:04 -0300

    add saved to games and series

--- db/db.sql
+++ db/db.sql
@@ -24,7 +24,8 @@ CREATE TABLE IF NOT EXISTS `projeto-native-games`.`users` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(200) NOT NULL,
   `pass` TEXT NOT NULL,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `unique-email` (`email` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -41,6 +42,7 @@ CREATE TABLE IF NOT EXISTS `projeto-native-games`.`games` (
   `finish` DATE NULL DEFAULT NULL,
   `platinum` DATE NULL DEFAULT NULL,
   `status` ENUM('Jogando', 'Zerado', 'Platinado') NULL DEFAULT NULL,
+  `saved` TINYINT NULL,
   `users_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`, `users_id`),
   INDEX `fk_games_users_idx` (`users_id` ASC),
@@ -65,6 +67,7 @@ CREATE TABLE IF NOT EXISTS `projeto-native-games`.`series` (
   `finish` DATE NULL DEFAULT NULL,
   `last_ep` VARCHAR(50) NULL DEFAULT NULL,
   `status` ENUM('Assistindo', 'Finalizado', 'Assistir mais tarde') NULL DEFAULT NULL,
+  `saved` TINYINT NULL,
   `users_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`, `users_id`),
   INDEX `fk_series_users1_idx` (`users_id` ASC),
@@ -76,16 +79,27 @@ CREATE TABLE IF NOT EXISTS `projeto-native-games`.`series` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
--- Inserir um usuário
-INSERT INTO `users` (`email`, `pass`) VALUES ('user@gmail.com', '123456');
 
--- Inserir jogos
-INSERT INTO `games` (`id`, `name`, `image`, `notes`, `start`, `finish`, `platinum`, `status`, `users_id`) VALUES
-(1, 'Batman Arkham knight', 'https://upload.wikimedia.org/wikipedia/pt/5/5d/Batman_Arkham_Knight_Capa.jpg?20140331090505', '', '2024-05-15', NULL, NULL, 'Platinado', 1),
-(2, 'God of war 2018', 'https://image.api.playstation.com/vulcan/img/rnd/202010/2217/p3pYq0QxntZQREXRVdAzmn1w.png', '', '2024-05-15', NULL, NULL, 'Zerado', 1),
-(3, 'Read dead redemption 2', 'https://image.api.playstation.com/cdn/UP1004/CUSA03041_00/Hpl5MtwQgOVF9vJqlfui6SDB5Jl4oBSq.png', '', '2024-05-15', NULL, NULL, 'Jogando', 1),
-(4, 'Aragami', 'https://image.api.playstation.com/cdn/UP1578/CUSA06168_00/aZ1WOSfMw9fsv4Y5KbT9ikS1THYP3egi.png', '', '2024-05-16', '2024-05-18', '2024-05-21', 'Platinado', 1),
-(5, 'Spider man remaster', 'https://image.api.playstation.com/vulcan/ap/rnd/202009/3021/B2aUYFC0qUAkNnjbTHRyhrg3.png', '', '2024-05-16', '2024-05-30', '2024-06-12', 'Platinado', 1);
+-- -----------------------------------------------------
+-- Table `projeto-native-games`.`session`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `projeto-native-games`.`session` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `userId` INT(11) NOT NULL,
+  `token` VARCHAR(1000) NOT NULL,
+  `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `session_token_key` USING HASH (`token`),
+  INDEX `userId` (`userId` ASC),
+  CONSTRAINT `session_userId_fkey`
+    FOREIGN KEY (`userId`)
+    REFERENCES `projeto-native-games`.`users` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
