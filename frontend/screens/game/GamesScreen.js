d81dd274c4b2ab7dbc88e4dffde038ecb8d77f60
lucas2007c <carneiro.g@aluno.ifsp.edu.br>
2024-05-23 20:58:06 -0300

    add logout if token expire in mainScreen

--- frontend/screens/game/GamesScreen.js
+++ frontend/screens/game/GamesScreen.js
@@ -1,4 +1,4 @@
-import { View, StyleSheet, ScrollView, FlatList, Activityndicator } from "react-native"
+import { View, StyleSheet, ScrollView, FlatList, Activityndicator, Modal } from "react-native"
 import { useState, useEffect } from "react"
 import H1 from "../../components/ui/H1"
 import axios from "axios"
@@ -12,12 +12,14 @@ import useGameStore from "../../stores/gameStore"
 import CardLoading from "../../components/CardLoading"
 import useUserLoggedStore from "../../stores/userLoggedStore"
 import { API_URL } from '../../constants/constants.js'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 
 const GamesScreen = () => {
     const { games, setGames } = useGameStore(state => state)
     const userLogged = useUserLoggedStore(state => state)
     const [loading, setLoading] = useState(true)
+    const [modalVisible, setModalVisible] = useState(false)
     const navigation = useNavigation()
 
     const getGames = async () => {
@@ -31,7 +33,27 @@ const GamesScreen = () => {
             setGames(data);
             setLoading(false)
         } catch (error) {
-            console.log(error);
+            if (error.response.status == 401) {
+                try {
+                    setModalVisible(true)
+                } catch (error) {
+                    console.log(error)
+                    alert('Erro ao fazer logout!')
+                }
+            }
+            console.log(error.response);
+        }
+    }
+
+    const handleLogout = async () => {
+        try {
+            await AsyncStorage.removeItem('userLogged')
+            userLogged.logout()
+            navigation.pop()
+            navigation.navigate('login')
+        } catch (error) {
+            console.log(error)
+            alert('Erro ao fazer logout!')
         }
     }
 
@@ -39,9 +61,9 @@ const GamesScreen = () => {
         getGames()
     }, [])
 
-    const jogando = games.filter((game) => game.status === 'Jogando')
-    const zerados = games.filter((game) => game.status === 'Zerado')
-    const platinados = games.filter((game) => game.status === 'Platinado')
+    const jogando = games?.filter((game) => game.status === 'Jogando')
+    const zerados = games?.filter((game) => game.status === 'Zerado')
+    const platinados = games?.filter((game) => game.status === 'Platinado')
 
     // const imageUrl = 'https://i.pinimg.com/474x/5f/b1/a9/5fb1a9dde21b1eda765156ba76676814.jpg'
 
@@ -51,11 +73,11 @@ const GamesScreen = () => {
                 <ScrollView>
 
                     <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                        <H1>Todos Os Jogos ({games.length})</H1>
+                        <H1>Todos Os Jogos ({games?.length})</H1>
                         <Button title='Novo' style={{ marginRight: 50 }} onPress={() => navigation.navigate('jogocadastro')} />
                     </View>
 
-                    {games.length > 0 ?
+                    {games?.length > 0 ?
                         <FlatList
                             data={games}
                             renderItem={({ item }) => <CardGame game={item} />}
@@ -68,8 +90,8 @@ const GamesScreen = () => {
                             <CardEmpty text='Nenhum jogo cadastrado' type='game' />
                     }
 
-                    <H1>Jogando ({jogando.length})</H1>
-                    {jogando.length > 0 ?
+                    <H1>Jogando ({jogando?.length})</H1>
+                    {jogando?.length > 0 ?
                         <FlatList
                             data={jogando}
                             renderItem={({ item }) => <CardGame game={item} />}
@@ -82,8 +104,8 @@ const GamesScreen = () => {
                             <CardEmpty text='Nenhum jogo sendo jogado' type='game' />
                     }
 
-                    <H1>Zerados ({zerados.length})</H1>
-                    {zerados.length > 0 ?
+                    <H1>Zerados ({zerados?.length})</H1>
+                    {zerados?.length > 0 ?
                         <FlatList
                             data={zerados}
                             renderItem={({ item }) => <CardGame game={item} />}
@@ -96,8 +118,8 @@ const GamesScreen = () => {
                             <CardEmpty text='Nenhum jogo zerado' type='game' />
                     }
 
-                    <H1>Platinados ({platinados.length})</H1>
-                    {platinados.length > 0 ?
+                    <H1>Platinados ({platinados?.length})</H1>
+                    {platinados?.length > 0 ?
                         <FlatList
                             data={platinados}
                             renderItem={({ item }) => <CardGame game={item} />}
@@ -109,6 +131,30 @@ const GamesScreen = () => {
                             :
                             <CardEmpty text='Nenhum jogo platinado' type='game' />
                     }
+
+                    <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={modalVisible}
+                        onRequestClose={() => {
+                            setModalVisible(false),
+                                handleLogout()
+                        }}>
+                        <View style={styles.centeredView}>
+                            <View style={styles.modalContainer}>
+                                <H1 style={{ fontSize: 20 }}>Sessão expirada, Faça o login novamente</H1>
+                                <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                                    <Button
+                                        title='Fechar'
+                                        onPress={() => {
+                                            setModalVisible(false)
+                                            handleLogout()
+                                        }}
+                                        style={{ padding: 10 }} />
+                                </View>
+                            </View>
+                        </View>
+                    </Modal>
                 </ScrollView>
             </ImageBackground>
         </View>
@@ -119,6 +165,20 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         // backgroundColor: COLORS.background,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    modalContainer: {
+        padding: 15,
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: COLORS.background,
+        borderColor: COLORS.primary,
+        borderWidth: 1
     }
 })
 
--- frontend/screens/serie/SeriesScreen.js
+++ frontend/screens/serie/SeriesScreen.js
@@ -1,4 +1,4 @@
-import { View, StyleSheet, ScrollView, FlatList, ActivityIndicator } from "react-native"
+import { View, StyleSheet, ScrollView, FlatList, ActivityIndicator, Modal } from "react-native"
 import { useState, useEffect } from "react"
 import H1 from "../../components/ui/H1"
 import axios from "axios"
@@ -12,12 +12,14 @@ import useSerieStore from "../../stores/serieStore"
 import CardLoading from "../../components/CardLoading"
 import useUserLoggedStore from "../../stores/userLoggedStore"
 import { API_URL } from '../../constants/constants.js'
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 
 const SerieScreen = () => {
     const { series, setSeries } = useSerieStore(state => state)
     const userLogged = useUserLoggedStore(state => state)
     const [loading, setLoading] = useState(true)
+    const [modalVisible, setModalVisible] = useState(false)
     const navigation = useNavigation()
 
     const getSerie = async () => {
@@ -31,7 +33,27 @@ const SerieScreen = () => {
             setSeries(data);
             setLoading(false)
         } catch (error) {
-            console.log(error);
+            if (error.response.status == 401) {
+                try {
+                    setModalVisible(true)
+                } catch (error) {
+                    console.log(error)
+                    alert('Erro ao fazer logout!')
+                }
+            }
+            console.log(error.response);
+        }
+    }
+
+    const handleLogout = async () => {
+        try {
+            await AsyncStorage.removeItem('userLogged')
+            userLogged.logout()
+            navigation.pop()
+            navigation.navigate('login')
+        } catch (error) {
+            console.log(error)
+            alert('Erro ao fazer logout!')
         }
     }
 
@@ -39,9 +61,9 @@ const SerieScreen = () => {
         getSerie()
     }, [])
 
-    const assistindo = series.filter((serie) => serie.status === 'Assistindo')
-    const finalizados = series.filter((serie) => serie.status === 'Finalizado')
-    const seeLater = series.filter((serie) => serie.status === 'Assistir_mais_tarde')
+    const assistindo = series?.filter((serie) => serie.status === 'Assistindo')
+    const finalizados = series?.filter((serie) => serie.status === 'Finalizado')
+    const seeLater = series?.filter((serie) => serie.status === 'Assistir_mais_tarde')
 
     return (
         <View style={{ flex: 1, backgroundColor: COLORS.secondary }}>
@@ -49,11 +71,11 @@ const SerieScreen = () => {
                 <ScrollView>
 
                     <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                        <H1>Todas As Séries ({series.length})</H1>
+                        <H1>Todas As Séries ({series?.length})</H1>
                         <Button title='Nova' style={{ marginRight: 50 }} onPress={() => navigation.navigate('seriecadastro')} />
                     </View>
 
-                    {series.length > 0 ?
+                    {series?.length > 0 ?
                         <FlatList
                             data={series}
                             renderItem={({ item }) => <CardSerie serie={item} />}
@@ -66,8 +88,8 @@ const SerieScreen = () => {
                             <CardEmpty text='Nenhuma série cadastrada' type='serie' />
                     }
 
-                    <H1>Assistindo ({assistindo.length})</H1>
-                    {assistindo.length > 0 ?
+                    <H1>Assistindo ({assistindo?.length})</H1>
+                    {assistindo?.length > 0 ?
                         <FlatList
                             data={assistindo}
                             renderItem={({ item }) => <CardSerie serie={item} />}
@@ -80,8 +102,8 @@ const SerieScreen = () => {
                             <CardEmpty text='Nenhuma série sendo assistida' type='serie' />
                     }
 
-                    <H1>Finalizadas ({finalizados.length})</H1>
-                    {finalizados.length > 0 ?
+                    <H1>Finalizadas ({finalizados?.length})</H1>
+                    {finalizados?.length > 0 ?
                         <FlatList
                             data={finalizados}
                             renderItem={({ item }) => <CardSerie serie={item} />}
@@ -94,8 +116,8 @@ const SerieScreen = () => {
                             <CardEmpty text='Nenhuma série finalizada' type='serie' />
                     }
 
-                    <H1>Assistir mais tarde ({seeLater.length})</H1>
-                    {seeLater.length > 0 ?
+                    <H1>Assistir mais tarde ({seeLater?.length})</H1>
+                    {seeLater?.length > 0 ?
                         <FlatList
                             data={seeLater}
                             renderItem={({ item }) => <CardSerie serie={item} />}
@@ -107,6 +129,29 @@ const SerieScreen = () => {
                             :
                             <CardEmpty text='Nenhuma série para assistir' type='serie' />
                     }
+                    <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={modalVisible}
+                        onRequestClose={() => {
+                            setModalVisible(false),
+                                handleLogout()
+                        }}>
+                        <View style={styles.centeredView}>
+                            <View style={styles.modalContainer}>
+                                <H1 style={{ fontSize: 20 }}>Sessão expirada, Faça o login novamente</H1>
+                                <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                                    <Button
+                                        title='Fechar'
+                                        onPress={() => {
+                                            setModalVisible(false)
+                                            handleLogout()
+                                        }}
+                                        style={{ padding: 10 }} />
+                                </View>
+                            </View>
+                        </View>
+                    </Modal>
                 </ScrollView>
             </ImageBackground>
         </View>
@@ -117,6 +162,20 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         // backgroundColor: COLORS.background,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    modalContainer: {
+        padding: 15,
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: COLORS.background,
+        borderColor: COLORS.primary,
+        borderWidth: 1
     }
 })
 
