c44de533018d7ca555d6752bb1bc9d0adfe5535c
lucas2007c <carneiro.g@aluno.ifsp.edu.br>
2024-05-23 22:40:17 -0300

    change token_expires time

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,5 +3,5 @@ export const HOST = process.env.HOST || 'http://localhost'
 
 export const SECRET_KEY = process.env.SECRET_KEY
 
-export const TOKEN_EXPIRES_IN = '30s'
+export const TOKEN_EXPIRES_IN = '1w'
 export const DB_TOKEN_EXPIRES_DAYS = 1 
